af009c32551e0c1fab31e583769894c17362ac0c
Fernando Gutiérrez <profesorfg@gmail.com>
2024-06-28 18:56:25 +0200

    Tia portal capturas y teoria

--- arduino/index_files/menu_general.js
+++ arduino/index_files/menu_general.js
@@ -1,10 +1,10 @@
 document.write(
 '<ul class="menu">',
-'  <li > <a style="background:pink" href="../index.html" >Volver a la WEB</a></li>',
-'  <li> <a href="../arduino/index.html" >arduino</a></li>',
-'  <li> <a href="tinkercad_Mblock.html" >Tinkercad Mblock</a></li>',
-'  <li> <a href="controlPorMovil.html">App inventor</a></li>',
-'  <li> <a href="arduino_Comprobacion_protoboard.html">Ejemplos C</a></li>',    
-'  <li> <a  style="background:pink" href="arduino_curso.html" >Volver a Curso ARDUINO</a></li>',
+'  <li > <a href="../index.html" >Volver a la WEB</a></li>',
+'  <li> <a style="background:#163" href="../arduino/index.html" >arduino</a></li>',
+'  <li> <a style="background:#163" href="tinkercad_Mblock.html" >Tinkercad Mblock</a></li>',
+'  <li> <a style="background:#163" href="controlPorMovil.html">App inventor</a></li>',
+'  <li> <a style="background:#163" href="arduino_Comprobacion_protoboard.html">Ejemplos C</a></li>',    
+'  <li> <a href="arduino_curso.html" >Volver a Curso ARDUINO</a></li>',
 '  <li> <a style="background:gray" href="https://cifpmedinadelcampo.es/iea/" >CIFP Electricidad</a></li>',
 );
